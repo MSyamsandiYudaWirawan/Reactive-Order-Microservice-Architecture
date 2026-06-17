a9eeb98bf292b8a310c8e940763e38ea3e192c9a
M.Syamsandi Yuda Wirawan <zxvcklh@gmail.com>
2026-06-17 08:47:34 +0700

    add config and infra

--- orchestrator-service/src/main/java/com/MSyamsandiYW/orchestrator_service/config/KafkaConfig.java
+++ orchestrator-service/src/main/java/com/MSyamsandiYW/orchestrator_service/config/KafkaConfig.java
@@ -1,9 +1,11 @@
 package com.MSyamsandiYW.orchestrator_service.config;
 
 import com.MSyamsandiYW.orchestrator_service.kafka.event.OrchestratorCommand;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.sender.KafkaSender;
@@ -11,20 +13,47 @@ import reactor.kafka.sender.SenderOptions;
 
 import java.util.List;
 
+import static com.MSyamsandiYW.orchestrator_service.properties.AppConstant.TOPICS.*;
+
 @Configuration
 public class KafkaConfig {
 
     @Bean
-    public KafkaSender<String,Object> kafkaSender(KafkaProperties kafkaProperties){
-        SenderOptions<String,Object> senderOptions = SenderOptions.create(kafkaProperties.buildProducerProperties());
+    public KafkaSender<String, Object> kafkaSender(KafkaProperties kafkaProperties) {
+        SenderOptions<String, Object> senderOptions = SenderOptions.create(kafkaProperties.buildProducerProperties());
         return KafkaSender.create(senderOptions);
     }
 
     @Bean
-    public KafkaReceiver<String, OrchestratorCommand> kafkaReceiver(KafkaProperties kafkaProperties){
-        ReceiverOptions<String,OrchestratorCommand> receiverOptions = ReceiverOptions.
+    public KafkaReceiver<String, OrchestratorCommand> kafkaReceiver(KafkaProperties kafkaProperties) {
+        ReceiverOptions<String, OrchestratorCommand> receiverOptions = ReceiverOptions.
                 <String, OrchestratorCommand>create(kafkaProperties.buildConsumerProperties())
-                .subscription(List.of());
+                .subscription(List.of(STOCK_RESERVE_COMPLETED, PAYMENT_INITIATED, PAYMENT_COMPLETED, PAYMENT_FAILED, OUT_OF_STOCK));
         return KafkaReceiver.create(receiverOptions);
+    }
+
+    @Bean
+    public NewTopic deductStock() {
+        return TopicBuilder.name(DEDUCT_STOCK).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic releaseStock() {
+        return TopicBuilder.name(RELEASE_STOCK).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic refundStock() {
+        return TopicBuilder.name(REFUND_REQUESTED).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic orderCompleted() {
+        return TopicBuilder.name(ORDER_COMPLETED).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic orderExpired() {
+        return TopicBuilder.name(ORDER_EXPIRED).partitions(3).replicas(1).build();
     }
 }
