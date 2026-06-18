cf6a3af8b60c07083bc3c5ce1c122198dafc26e6
M.Syamsandi Yuda Wirawan <zxvcklh@gmail.com>
2026-06-18 08:44:53 +0700

    refactor zoneddatetime to instant for entity and storage

--- auth-service/src/main/java/com/MSyamsandiYW/auth_service/config/R2dbcConfig.java
+++ auth-service/src/main/java/com/MSyamsandiYW/auth_service/config/R2dbcConfig.java
@@ -1,44 +1,9 @@
 package com.MSyamsandiYW.auth_service.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.convert.ReadingConverter;
-import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
-import org.springframework.data.r2dbc.dialect.PostgresDialect;
-
-import java.time.OffsetDateTime;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableR2dbcAuditing(auditorAwareRef = "reactiveAuditorAware")
 public class R2dbcConfig {
-
-    @Bean
-    public R2dbcCustomConversions r2dbcCustomConversions() {
-        List<Converter<?, ?>> converters = new ArrayList<>();
-        converters.add(new ZonedDateTimeReadConverter());
-        converters.add(new ZonedDateTimeWriteConverter());
-        return R2dbcCustomConversions.of(PostgresDialect.INSTANCE, converters);
-    }
-
-    @ReadingConverter
-    static class ZonedDateTimeReadConverter implements Converter<OffsetDateTime, ZonedDateTime> {
-        @Override
-        public ZonedDateTime convert(OffsetDateTime source) {
-            return source.toZonedDateTime();
-        }
-    }
-
-    @WritingConverter
-    static class ZonedDateTimeWriteConverter implements Converter<ZonedDateTime, OffsetDateTime> {
-        @Override
-        public OffsetDateTime convert(ZonedDateTime source) {
-            return source.toOffsetDateTime();
-        }
-    }
 }
